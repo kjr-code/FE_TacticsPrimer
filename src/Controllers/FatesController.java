5d38554b940c956e804072dcc16b975d3678a858
kjr-code <kjr-code@gmail.com>
2022-11-01 15:50:18 -0400

    minor adjustments & start work on 'fates'

--- src/Controllers/FatesController.java
+++ src/Controllers/FatesController.java
@@ -1,5 +1,33 @@
 package Controllers;
 
-public class FatesController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class FatesController implements Initializable {
+
+    @FXML
+    private Button addUnitButton;
+
+    @FXML
+    private Label noneSelectedLabel;
+
+    @FXML
+    private VBox unitDataDisplay;
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1){
+        //set all of the "unit slots" to disabled on initialize
+        //re-init when they're enabled by the "add a unit" button
+    }
+
+    private void addUnit(){
+        //bring up "add unit" menu in a seperate "box"
+    }
     
 }
