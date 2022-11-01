9b884083d8f573812f1107d0dad78966e7b17888
kjr-code <kjr-code@gmail.com>
2022-11-01 15:53:40 -0400

    Small adjustments & begin work on Fates fxml

--- src/Controllers/GameSelectController.java
+++ src/Controllers/GameSelectController.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.image.ImageView;
 
@@ -12,6 +13,9 @@ public class GameSelectController implements Initializable {
     
     @FXML
     private ChoiceBox<String> gameBox;
+
+    @FXML
+    private Button goButton;
 
     @FXML
     private ImageView menuLogo; 
@@ -22,5 +26,10 @@ public class GameSelectController implements Initializable {
     public void initialize(URL arg0, ResourceBundle arg1){
         gameBox.getItems().addAll(games);
         gameBox.setValue("Fire Emblem: Fates");
+    }
+
+    private void makeSelection(String selectedGame){
+        //TODO passing in a String feels suboptimal here
+        
     }
 }
